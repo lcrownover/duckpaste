159ff0f72ddbead194cbfad5ffc2ed8cba154ae4
Lucas Crownover <lcrownover127@gmail.com>
2024-01-02 12:13:33 -0800

    init frontent

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>
+    <span class="new-paste"> New Paste </span>
+  </h1>
+
+  <form>
+    <textarea rows="10"></textarea>
+    <input type="radio" id="hours1" name="hours1" value="hours1" /><span
+      id="hours1">1 hour</span
+    >
+    <input type="radio" id="hours24" name="hours24" value="hours24" /><span
+      id="hours24">24 hours</span
+    >
+    <input type="radio" id="hours48" name="hours48" value="hours48" /><span
+      id="hours48">48 hours</span
+    >
+    <input type="radio" id="custom" name="custom" value="custom" /><span
+      id="custom">Custom</span
+    >
+    <input type="text" id="custom-hours" name="custom-hours" value="8" /><span
+      id="custom-hours">hours</span
+    >
+    <input
+      type="radio"
+      id="delete-on-read"
+      name="delete-on-read"
+      value="delete-on-read"
+    /><span id="delete-on-read">Delete on Read</span>
+    <button>Save</button>
+  </form>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: left;
+    flex: 0.6;
+  }
+
+  h1 {
+    width: 100%;
+  }
+
+  .new-paste {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    height: 64rem;
+  }
+
+  textarea {
+    height: auto;
+  }
+</style>
---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,99 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/duck.png';
+	import github from '$lib/images/github-white.png';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://uoregon.edu">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/lcrownover/duckpaste" target="_blank" rel="noopener noreferrer">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+        background: linear-gradient(0deg, #007030 25%, #104735 135%);
+        padding-top: 20px;
+        padding-bottom: 20px;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 1rem;
+		/* text-transform: uppercase; */
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-3);
+	}
+</style>
