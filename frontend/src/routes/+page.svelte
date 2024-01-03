ccc6a6a25844aa67db1683207affc50e820a075b
Lucas Crownover <lcrownover127@gmail.com>
2024-01-02 20:59:16 -0800

    more work on the gui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -13,43 +13,36 @@
 
   <main>
     <section>
-      <h1>
-        <span class="new-paste"> New Paste </span>
-      </h1>
-
       <form>
         <div class="row">
           <div class="column">
             <h2>Options</h2>
-            <div id="paste-options">
-              <input type="radio" id="hours1" name="hours_valid" value="1" />
-              <label for="hours1">1 hour</label>
-              <input type="radio" id="hours24" name="hours_valid" />
-              <label for="24">24 hours</label>
-              <input type="radio" id="hours48" name="hours_valid" value="48" />
-              <label for="hours48">48 hours</label>
-              <input type="radio" id="custom" name="hours_valid" />
-              <label for="custom">Custom</label>
-              <input
-                type="text"
-                id="custom-hours"
-                name="hours_valid"
-                value="8"
-              />
-              <label for="custom-hours">hours</label>
-              <input
-                type="radio"
-                id="delete-on-read"
-                name="hours_valid"
-                value="0"
-              />
-              <label for="delete-on-read">Delete on Read</label>
-              <button>Save</button>
+
+            <div class="paste-options">
+              <div class="options">
+                <label for="options">paste expiration</label>
+                <select class="select-options" name="paste-options">
+                  <option value="48">1 Week</option>
+                  <option value="48">48 Hours</option>
+                  <option value="24">24 Hours</option>
+                  <option value="8">8 Hours</option>
+                  <option value="1">1 Hour</option>
+                </select>
+              </div>
+
+              <div class="options">
+                <label for="burn">delete upon reading</label>
+                <input type="checkbox" class="burn" name="burn" />
+              </div>
+
+              <div class="submit">
+                <button>Save</button>
+              </div>
             </div>
           </div>
           <div class="column">
             <div id="paste-input">
-              <textarea rows="32"></textarea>
+              <textarea cols="80" rows="32"></textarea>
             </div>
           </div>
         </div>
